c83d0ad81b86f9d88d5584b48021f163c2017f26
meriem-mejri <meriemmmejri@gmail.com>
2025-03-25 03:06:50 +0100

    add/remove/list/single product

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,12 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from "./routes/userRoute.js";
+import productRouter from './routes/productRoute.js'
 
 //app config (create instance of express server )
 const app = express()
 const port = process.env.PORT || 4000
+
 console.log("MongoDB URI:", process.env.MONGODB_URL);
 
 connectDB()
@@ -15,10 +17,11 @@ connectCloudinary()
 
 //middlewares
 app.use(express.json())
-app.use(cors())  //(we can access the backend from any api)
+app.use(cors())  // (we can access the backend from any api)
 
 //api endpoints
 app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
 
 app.get('/',(req,res)=>{
     res.send("API Working")
